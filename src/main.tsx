2cdafc986e29d8362c29f77e79a59010dd2c7d00
Jeral Pineda <jpineda@banhcafe.hn>
2023-07-06 16:24:49 -0600

    feature: Creando el contexto de lugares

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { MapsApp } from './MapsApp.tsx';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <MapsApp />
+  </React.StrictMode>
+);
