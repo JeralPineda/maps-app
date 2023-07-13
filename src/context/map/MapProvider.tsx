f44e4aff95a62d1df79322505a74987053791208
Jeral Pineda <jpineda@banhcafe.hn>
2023-07-13 15:19:19 -0600

    feature: Colocar marcadores en cada lugar encontrado

--- src/context/map/MapProvider.tsx
+++ src/context/map/MapProvider.tsx
@@ -1,17 +1,20 @@
-import { useReducer } from 'react';
+import { useReducer, useContext, useEffect } from 'react';
 import { Map, Marker, Popup } from 'mapbox-gl';
 
 import { MapContext } from './MapContext';
 import { mapReducer } from './mapReducer';
+import { PlacesContext } from '..';
 
 export interface MapState {
   isMapReady: boolean;
   map?: Map;
+  markers: Marker[];
 }
 
 const INITIAL_STATE: MapState = {
   isMapReady: false,
   map: undefined,
+  markers: [],
 };
 
 export interface Props {
@@ -20,6 +23,32 @@ export interface Props {
 
 export const MapProvider = ({ children }: Props) => {
   const [state, dispatch] = useReducer(mapReducer, INITIAL_STATE);
+  const { places } = useContext(PlacesContext);
+
+  useEffect(() => {
+    state.markers.forEach((marker) => marker.remove()); //limpiar marcadores viejos
+    const newMarkers: Marker[] = [];
+
+    for (const place of places) {
+      const [lng, lat] = place.center;
+      const popup = new Popup().setHTML(`
+                    <h6>${place.text_es}</h6>
+                    <p>${place.place_name_es}</p>
+                `);
+
+      const newMarker = new Marker()
+        .setPopup(popup)
+        .setLngLat([lng, lat])
+        .addTo(state.map!);
+
+      newMarkers.push(newMarker);
+    }
+
+    // Todo: limpiar polyline
+
+    dispatch({ type: 'setMarkers', payload: newMarkers });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [places]);
 
   const setMap = (map: Map) => {
     const myLocationPopup = new Popup().setHTML(`
